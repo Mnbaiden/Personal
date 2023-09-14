192658aab1c99d0fa930d0fa3ae0562993aefc03
mnbaiden <mickbaiden@gmail.com>
2023-09-14 10:14:11 -0700

    task one

--- simple_shell/_atoi.c
+++ simple_shell/_atoi.c
@@ -10,11 +10,14 @@
  */
 int interactive(info_t *info)
 {
-    /* Check if STDIN is associated with a terminal and if readfd is less than or equal to 2.*/
-    if (isatty(STDIN_FILENO) && info->readfd <= 2)
-        return 1; /* Interactive mode */
-    else
-        return 0; /* Not in interactive mode */
+/**
+ * Check if STDIN is associated with a terminal and
+ * if readfd is less than or equal to 2.
+ */
+	if (isatty(STDIN_FILENO) && info->readfd <= 2)
+		return (1); /* Interactive mode */
+	else
+		return (0); /* Not in the interactive mode */
 }
 
 /**
@@ -26,13 +29,13 @@ int interactive(info_t *info)
  */
 int is_delim(char c, char *delim)
 {
-    while (*delim)
-    {
-        if (*delim == c)
-            return 1; /* Character is a delimiter */
-        delim++;
-    }
-    return 0; /* Character is not a delimiter */
+	while (*delim)
+	{
+		if (*delim == c)
+			return (1); /* Character is a delimiter */
+		delim++;
+	}
+	return (0); /* character is not a delimiter */
 }
 
 /**
@@ -43,10 +46,10 @@ int is_delim(char c, char *delim)
  */
 int _isalpha(int c)
 {
-    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-        return 1; /* Character is alphabetic */
-    else
-	return (0); /* Character is not alphabetic */
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1); /* Character is alphabetic */
+	else
+		return (0); /* Character is not alphabetic */
 }
 
 /**
@@ -57,34 +60,30 @@ int _isalpha(int c)
  */
 int _atoi(char *s)
 {
-    int i = 0;
-    int sign = 1;
-    int flag = 0;
-    int output = 0;
+	int i = 0;
+	int sign = 1;
+	int flag = 0;
+	int output = 0;
+	unsigned int result = 0;
 
-    unsigned int result = 0;
+	while (s[i] != '\0' && flag != 2)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
 
-    while (s[i] != '\0' && flag != 2)
-    {
-        if (s[i] == '-')
-            sign *= -1;
-
-        if (s[i] >= '0' && s[i] <= '9')
-        {
-            flag = 1;
-            result *= 10;
-            result += (s[i] - '0');
-        }
-        else if (flag == 1)
-            flag = 2;
-
-        i++;
-    }
-
-    if (sign == -1)
-        output = -result;
-    else
-        output = result;
-
-    return output;
+		i++;
+	}
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+	return (output);
 }
